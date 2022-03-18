5eb5c3f9f3ec2182f6a0a2cc24ef2097f4515614
Md Shihab Uddin <uddinmdshihab452@gmail.com>
2022-03-18 10:05:35 +0600

    add module 45-5

---
+++ module-46-5/practice/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import Practice from './Componant/Practice-1-8/Practice';
+
+function App() {
+  return (
+    <div className="App">
+     <Practice></Practice>
+    </div>
+  );
+}
+
+export default App;
