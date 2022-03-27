edaea51341895152549a6672e279730e7f3ed76f
Md Shihab Uddin <uddinmdshihab452@gmail.com>
2022-03-27 20:09:33 +0600

    local

---
+++ detiles-local-storage/module-50-5-1-to-3/index.js
@@ -0,0 +1,16 @@
+/*************************module 50-5-1***************************************/
+const db={}
+const addToDb=item=>{
+    // 1. set data if given data
+    // db.alom= 1;
+
+    //2. set data if given data
+    // db['allom']=1
+
+    //set data if data can not given
+db[item] =1
+
+    console.log(db)
+}
+// addToDb()
+addToDb('hero')
